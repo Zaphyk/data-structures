722058b900f7a6aac04f83491d8fa0ad70582da1
Zaphyk <zaphyk@live.com>
2017-11-08 09:58:47 -0300

    removed *_tests.py and work on linked list

--- linked_list.py
+++ linked_list.py
@@ -1,2 +1,45 @@
 class Node:
+    def __init__(self, item):
+        self.value = item
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.first = None
+
+    def AddLast(self, _item):
+        item = Node(_item)
+        if self.first == None:
+            self.first = item
+        else:
+            node = self.first
+
+            while(node.next != None):
+                node = node.next
+            node.next = item
     
+    def AddFirst(self, _item):
+        item = Node(_item)
+        tmp = self.first
+        self.first = item
+        item.next = tmp
+
+    def RemoveFirst(self):
+        Remove(self.first)
+
+    def Remove(self, node):
+        tmp = node.next
+        prev = self.first
+        if self.first == node:
+            self.first = tmp
+        else:
+            while(prev.next != node):
+                    prev = prev.next
+            prev.next = tmp
+        
+
+    
+
+        
+        
